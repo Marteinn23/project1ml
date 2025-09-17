1a1c351374987558637d54838bd13953bf91e689
Marteinn23 <marteinn23@ru.is>
2025-09-17 15:56:47 +0000

    second commit

--- project1.py
+++ project1.py
@@ -1,1 +1,7 @@
-print("hello world")
+import pandas as pd
+
+data = pd.read_csv("dataset/PhiUSIIL_Phishing_URL_Dataset.csv")
+
+data = data.drop(columns=["FILENAME","URLLength","DomainLength",""])
+
+print(data.keys())
